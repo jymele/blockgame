3afa49f0bc11ca078f206aea5f75614665b8e76f
Joel Ymele Leki <jymele@tfo.org>
2025-04-21 14:47:29 -0400

    Added version

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Experience from "@/experience/experience";
 export default function Home() {
   return (
     <main className="p-4 w-fit mx-auto">
+      <div>Version: 0.01</div>
       <Experience />
     </main>
   );
