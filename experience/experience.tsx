d890a2c4c04f029f16b4ddac3c9327972167563f
Joel Ymele Leki <jymele@tfo.org>
2025-04-21 09:18:31 -0400

    Animation properly acts once per frame

--- experience/experience.tsx
+++ experience/experience.tsx
@@ -1,21 +1,18 @@
 "use client";
 import GameBoard from "@/components/board/gameboard";
 import { useEffect, useState } from "react";
-import { animate } from "animejs";
+import { createTimer } from "animejs";
 import Block from "./classes/block";
 
 export default function Experience() {
-  //   const [block, setBlock] = useState<Block>(new Block());
-  //   const block = new Block();
   const [blockShape, setBlockShape] = useState<number[]>([]);
 
   useEffect(() => {
     const block = new Block();
 
-    const animation = animate(".tile", {
+    createTimer({
       loop: true,
-      fps: 1,
-      duration: 1000,
+      frameRate: 1,
       onUpdate: () => {
         block.goDown();
         setBlockShape(block.getShape());
