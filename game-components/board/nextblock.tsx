e9dcb92c5b0b97a421d3ad64282e63b9f1c3293f
Joel Ymele Leki <jymele@tfo.org>
2025-05-01 10:16:33 -0400

    Displays the shape of the next block

--- game-components/board/nextblock.tsx
+++ game-components/board/nextblock.tsx
@@ -1,28 +1,70 @@
-import { useEffect } from "react";
+"use client";
+import clsx from "clsx";
+import { useEffect, useState } from "react";
 
 type NextBlockProps = {
   block?: string;
 };
 
+const rows = 3;
+const columns = 4;
+
+const oDisplay: number[] = [1, 2, columns + 1, columns + 2];
+const iDisplay: number[] = [0, 1, 2, 3];
+const tDisplay: number[] = [1, columns, columns + 1, columns + 2];
+const lDisplay: number[] = [columns, columns + 1, columns + 2, 2];
+const jDisplay: number[] = [0, columns, columns + 1, columns + 2];
+const sDisplay: number[] = [1, columns, columns + 1, columns + 2];
+const zDisplay: number[] = [0, columns, columns + 1, columns + 2];
+
 export default function NextBlock(props: NextBlockProps) {
-  const rows = 3;
-  const columns = 4;
   const { block } = props;
   const numbers = Array.from({ length: rows * columns }, (_, i) => i + 1);
+  const [blockShape, setBlockShape] = useState<number[]>([]);
 
   useEffect(() => {
-    // This effect runs when the block prop changes
-    // console.log("Next block changed:", block);
+    switch (block) {
+      case "o":
+        setBlockShape(oDisplay);
+        break;
+      case "i":
+        setBlockShape(iDisplay);
+        break;
+      case "t":
+        setBlockShape(tDisplay);
+        break;
+      case "l":
+        setBlockShape(lDisplay);
+        break;
+      case "j":
+        setBlockShape(jDisplay);
+        break;
+      case "s":
+        setBlockShape(sDisplay);
+        break;
+      case "z":
+        setBlockShape(zDisplay);
+        break;
+      default:
+        setBlockShape([]); // Clear the shape if block is not recognized
+    }
   }, [block]);
 
   return (
     <div>
-      <div className="grid bg-white grid-cols-4 w-fit rounded shadow p-2">
-        {numbers.map((number) => (
-          <div key={number} className="next-tile"></div>
+      {/* Next Block: {block} */}
+      <p>Next block</p>
+      <div className="grid bg-white grid-cols-4 w-fit rounded gap-1 shadow p-2">
+        {numbers.map((number, index) => (
+          <div
+            key={index}
+            className={clsx([
+              "next-tile",
+              blockShape.includes(index) ? "active-tile" : "",
+            ])}
+          ></div>
         ))}
       </div>
-      Next Block: {block}
     </div>
   );
 }
